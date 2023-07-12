b3a4545f3b83adffbd7b434491ddfb4f21599c03
Alptekin <alptekin.ocakdan@gmail.com>
2023-07-12 11:38:28 +0300

    Prisma, PlanetScale, MySQL setup (Admin)

--- server/components/modals/store-modal.tsx
+++ server/components/modals/store-modal.tsx
@@ -9,41 +9,56 @@ import {zodResolver} from "@hookform/resolvers/zod";
 import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
+import {useState} from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
-    name:z.string().min(1),
+    name: z.string().min(1),
 })
 
 export const StoreModal = () => {
     const storeModal = useStoreModal();
+    const [loading, setLoading] = useState(false);
     const form = useForm<z.infer<typeof formSchema>>({
-        resolver:zodResolver(formSchema),
-        defaultValues:{
-            name:"",
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
         }
     });
-    const onSubmit = async (values:z.infer<typeof formSchema>)=>{
-        console.log(values);
-        // TODO: Create Store
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            setLoading(true);
+
+            const response = await axios.post('/api/stores',values);
+            toast.success("Store Created");
+        } catch (error) {
+            toast.error("Something went wrong.");
+        }finally {
+            setLoading(false);
+        }
     }
     return (
-        <Modal title="Create Store" description="Add a new store to manage products and categories" isOpen={false} onClose={() => {}}>
+        <Modal title="Create Store" description="Add a new store to manage products and categories" isOpen={false}
+               onClose={() => {
+               }}>
             <div>
                 <div className="space-y-4 py-2 pb-4">
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)}>
-                            <FormField control={form.control} name="name" render={({field})=>(
+                            <FormField control={form.control} name="name" render={({field}) => (
                                 <FormItem>
                                     <FormLabel>Name</FormLabel>
                                     <FormControl>
-                                        <Input placeholder="E-Commmerce" {...field}/>
+                                        <Input disabled={loading} placeholder="E-Commmerce" {...field}/>
                                     </FormControl>
                                     <FormMessage/>
                                 </FormItem>
                             )}/>
                             <div className="pt-6 space-x-2 items-center justify-end w-full">
-                                <Button variant="outline" onClick={storeModal.onClose}>Cancel</Button>
-                                <Button type="submit">Continue</Button>
+                                <Button disabled={loading} variant="outline"
+                                        onClick={storeModal.onClose}>Cancel</Button>
+                                <Button disabled={loading} type="submit">Continue</Button>
                             </div>
                         </form>
                     </Form>
