76e6110f9e78b587960838262d798cf75ecc0b82
Alptekin <alptekin.ocakdan1@ogr.sakarya.edu.tr>
2024-01-18 23:49:47 +0300

    Preset changes

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="gvx66luj">
         {({ open }) => {
           const onClick = () => {
             open();
